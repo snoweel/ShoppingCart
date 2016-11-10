a8f8fcebbffce0999f6999cdd21e7eb51f4df42e
snoweel pereira <snow.pereira@gmail.com>
2016-11-10 22:23:23 +0530

    made max Qty larger

--- src/client/app/cart/editItemInModal.controller.js
+++ src/client/app/cart/editItemInModal.controller.js
@@ -10,6 +10,8 @@
   function EditItemInModalController($scope, $mdDialog,item,product) {
     console.log(`item : ${item}`);
     console.log(`product : ${product}`);
+    var MAX_BUY_COUNT = 50;
+    $scope.Qty = _.range(1,MAX_BUY_COUNT);
 
     // var vm = this;
     $scope.item =angular.copy(item);
